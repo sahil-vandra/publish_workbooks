f6a9b68853cfcdb79c616faeccaf39e97aed813f
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-09-27 18:59:23 +0530

    a

--- publish_workbook.py
+++ publish_workbook.py
@@ -4,43 +4,31 @@ import json
 
 
 def main(args):
-    print("\nproject_id ::", args.project_id)
-    print("\ntype of project_id ::", type(args.project_id))
-    a = json.loads(args.project_id)
-    print('\na ::', a)
-    print('\ntype of a ::', type(a))
+    temp_var = json.loads(args.project_id)
+    print('\na ::', temp_var)
+    print('\ntype of a ::', type(temp_var))
     
-    # wb_list = []
-    # for wb in args.workbook_files.split(","):
-    #     temp_wb = wb.strip()
-    #     if len(temp_wb) > 0:
-    #         wb_list.append(temp_wb)
-    # project_id = args.project_id.replace('"', "")
+    # Step 1: Sign in to server.
+    tableau_auth = TSC.TableauAuth(args.username, args.password)
+    server = TSC.Server(args.server_url)
+    overwrite_true = TSC.Server.PublishMode.Overwrite
 
-    # if len(wb_list) > 0:
-    #     # Step 1: Sign in to server.
-    #     tableau_auth = TSC.TableauAuth(args.username, args.password)
-    #     server = TSC.Server(args.server_url)
-    #     overwrite_true = TSC.Server.PublishMode.Overwrite
+    with server.auth.sign_in(tableau_auth):
+        for data in temp_var:
+            # Step 2: Get all the projects on server, then look for the default one.
+            all_projects, pagination_item = server.projects.get()
+            project = next(
+                (project for project in all_projects if project.name == data['project_path']), None)
 
-    #     with server.auth.sign_in(tableau_auth):
-    #         # Step 2: Get all the projects on server, then look for the default one.
-    #         all_projects, pagination_item = server.projects.get()
-    #         project = next(
-    #             (project for project in all_projects if project.name == args.project_name), None)
-
-    #         # Step 3: If default project is found, form a new workbook item and publish.
-    #         if project is not None:
-    #             for wb_file in wb_list:
-    #                 new_workbook = TSC.WorkbookItem(project_id)
-    #                 new_workbook = server.workbooks.publish(
-    #                     new_workbook, wb_file, overwrite_true)
-    #                 print(f"\nWorkbook :: {wb_file} :: published")
-    #         else:
-    #             error = "The project could not be found."
-    #             raise LookupError(error)
-    # else:
-    #     print("Workbook list is null")
+            # Step 3: If default project is found, form a new workbook item and publish.
+            if project is not None:
+                new_workbook = TSC.WorkbookItem(project.id)
+                new_workbook = server.workbooks.publish(
+                    new_workbook, data['file_path'], overwrite_true)
+                print(f"\nWorkbook :: {data['file_path']} :: published in {data['project_path']} project")
+            else:
+                error = "The project could not be found."
+                raise LookupError(error)
 
 
 if __name__ == '__main__':
@@ -53,7 +41,25 @@ if __name__ == '__main__':
                         type=str, required=True)
     parser.add_argument('--project_id', action='store',
                         type=str, required=True)
-    parser.add_argument('--workbook_files', action='store',
-                        type=str, required=True)
     args = parser.parse_args()
     main(args)
+
+
+# [
+#     {
+#         "name": "film_workbook",
+#         "file_path": "film_workbook.twb",
+#         "project_path": "Technology",
+#     },
+#     {
+#         "name": "nz_venki",
+#         "file_path": "NZ_Census.twbx",
+#         "project_path": "Default",
+#         "option": {
+#             "tags": ["sample", "temp"],
+#             "hidden_views": ["Event by day"],
+#             "show_tabs": True,
+#             "description": "Sample description",
+#         },
+#     },
+# ]
