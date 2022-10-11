594b7a4864bdff7f1d2f5e9c9cf9f03f69c6d677
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-10-11 18:56:00 +0530

    a

--- publish_workbook.py
+++ publish_workbook.py
@@ -25,7 +25,7 @@ def main(args):
                     new_workbook = TSC.WorkbookItem(
                         name=data['name'], project_id=project.id, show_tabs=data['show_tabs'])
                     new_workbook = server.workbooks.publish(
-                        new_workbook, data['file_path'], 'Overwrite', hidden_views=data['hidden_views'], as_job=True)
+                        new_workbook, data['file_path'], 'Overwrite', hidden_views=data['hidden_views'])
                     if data['tags'] is not None:
                         new_workbook.tags = set(data['tags'])
                         new_workbook = server.workbooks.update(
