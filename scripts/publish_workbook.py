6b8dec059633cf75518af2d32bce9ae4b071ca98
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-10-13 14:49:25 +0530

    a

--- scripts/publish_workbook.py
+++ scripts/publish_workbook.py
@@ -14,7 +14,6 @@ def main(args):
         with server.auth.sign_in(tableau_auth):
             for data in project_data_json:
                 workbook_file_path = data['file_path']
-                
                 wb_path = "/home/runner/work/publish_workbooks/publish_workbooks/workbooks/" + data['file_path']
                 print("wb_path: ", wb_path)
                 
