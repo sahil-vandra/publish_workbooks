60cf4a5acf87de8f5ec88b6978b498d9613bd3ec
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-10-13 14:48:29 +0530

    a

--- scripts/publish_workbook.py
+++ scripts/publish_workbook.py
@@ -15,7 +15,7 @@ def main(args):
             for data in project_data_json:
                 workbook_file_path = data['file_path']
                 
-                wb_path = "./workbooks/" + data['file_path']
+                wb_path = "/home/runner/work/publish_workbooks/publish_workbooks/workbooks/" + data['file_path']
                 print("wb_path: ", wb_path)
                 
                 if data['project_path'] is None:
