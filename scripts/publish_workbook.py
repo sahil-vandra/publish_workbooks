baf24d6d03ac02c96bf3ced15203fa92e68c34fb
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-10-31 11:41:49 +0530

    New Commit

--- scripts/publish_workbook.py
+++ scripts/publish_workbook.py
@@ -9,7 +9,7 @@ def main(args):
     try:
         for data in project_data_json:
             # Step 1: Sign in to server.
-            tableau_auth = TSC.TableauAuth(args.username, args.password, site_id='https://tableau.devinvh.com/#/site/DataLab/projects')
+            tableau_auth = TSC.TableauAuth(args.username, args.password, site_id='e4fc3628-68b4-4a72-a956-9fc3c02192f4')
             server = TSC.Server(args.server_url)
 
             with server.auth.sign_in(tableau_auth):
