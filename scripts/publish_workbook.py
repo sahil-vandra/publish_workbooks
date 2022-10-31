c7b833b6a22b8e2408ff2752887d2be6badffcf5
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-10-31 16:14:11 +0530

    New Commit

--- scripts/publish_workbook.py
+++ scripts/publish_workbook.py
@@ -18,7 +18,7 @@ def main(args):
 
                 all_project_items, pagination_item = server.projects.get()
                 project_item = all_project_items[0]
-                print("project_item", project_item)
+                print("project_item", all_project_items[0])
 
                 capabilities = {
                     TSC.Permission.Capability.ViewComments: TSC.Permission.Mode.Allow
